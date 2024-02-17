42facb792452b6bd67a04648d599d4033f721cd9
Andrey <shilovish_AV@tut.by>
2024-02-17 22:36:47 +0300

    Initial commit

---
+++ src/main/java/com/example/builderit/BuilderitApplication.java
@@ -0,0 +1,13 @@
+package com.example.builderit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilderitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuilderitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/builderit/BuilderitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.builderit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilderitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
